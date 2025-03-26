f2b8ba1820117f76255406a60ce51c62f9542960
MartiHed <martyna.hedeszynska@gmail.com>
2025-03-26 22:16:12 +0100

    poprawilam czytelnosc mojego kodu i podzielilam go na mniejsze czesci. dodatkowo pozmienialam kolory

--- lib/pages/HangmanGame.dart
+++ lib/pages/HangmanGame.dart
@@ -1,7 +1,9 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
+import '../Widgets/HangmanWidgets/GameOverScreen.dart';
+import '../Widgets/HangmanWidgets/Keyboard.dart';
+import '../Widgets/HangmanWidgets/LivesDisplay.dart';
+import '../Widgets/HangmanWidgets/WordDisplay.dart';
+import '../appLogic/HangmanLogic.dart';
 
 class HangmanGame extends StatefulWidget {
   @override
@@ -9,59 +11,17 @@ class HangmanGame extends StatefulWidget {
 }
 
 class _HangmanGameState extends State<HangmanGame> {
-  Set<String> wordsToGuess = {"FLUTTER","KOPALNIA", "CZLOWIEK"};
-  late List<String> wordGuessed;
-  int lives = 1;
-  bool isGameOver = false;
-  bool isWinner = false;
-  String wordToGuessByUser = "";
+  final HangmanLogic _game = HangmanLogic();
 
-  @override
-  void initState() {
-    super.initState();
-
-
-    var rnd = Random();
-
-    int rndIndex = rnd.nextInt(wordsToGuess.length);
-    wordToGuessByUser = wordsToGuess.elementAt(rndIndex);
-    lives = wordToGuessByUser.length;
-    wordGuessed = List.generate(wordToGuessByUser.length, (index) => "*");
-  }
-
-  void guessLetter(String letter) {
-    if (isGameOver) return;
-
+  void _guessLetter(String letter) {
     setState(() {
-      bool found = false;
-      for (int i = 0; i < wordToGuessByUser.length; i++) {
-        if (wordToGuessByUser[i] == letter) {
-          wordGuessed[i] = letter;
-          found = true;
-        }
-      }
-      if (!found) {
-        lives--;
-      }
-      if (!wordGuessed.contains("*")) {
-        isGameOver = true;
-        isWinner = true;
-      } else if (lives <= 0) {
-        isGameOver = true;
-        isWinner = false;
-      }
+      _game.guessLetter(letter);
     });
   }
 
-  void resetGame() {
+  void _resetGame() {
     setState(() {
-      var rnd = Random();
-      int rndIndex = rnd.nextInt(wordsToGuess.length);
-      wordToGuessByUser = wordsToGuess.elementAt(rndIndex);
-      wordGuessed = List.generate(wordToGuessByUser.length, (index) => "*");
-      lives = wordToGuessByUser.length;
-      isGameOver = false;
-      isWinner = false;
+      _game.resetGame();
     });
   }
 
@@ -75,96 +35,19 @@ class _HangmanGameState extends State<HangmanGame> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                RichText(
-                  text: TextSpan(
-                    style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 2,
-                      ),
-                    ),
-                    children: [
-                      TextSpan(text: "Życia: $lives "),
-                      WidgetSpan(
-                        alignment: PlaceholderAlignment.middle,
-                        child: Image.asset(
-                          'images/mcHeart.png',
-                          width: 30,
-                          height: 30,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                SizedBox(height: 20),
-                Text(
-                  wordGuessed.join(" "),
-                  style: GoogleFonts.poppins(
-                    textStyle: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Color(0xFfefa00b)),
-                  ),
-                ),
+                LivesDisplay(lives: _game.lives),
+                WordDisplay(wordGuessed: _game.wordGuessed),
                 SizedBox(height: 40),
-                Wrap(
-                  spacing: 12.0,
-                  runSpacing: 10.0,
-                  children: 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŻŹ'.split('').map((letter) {
-                    return ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF0075c4),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          side: BorderSide(
-                            color: Colors.white,
-                            width: 2, // Border thickness
-                          ),
-                        ),
-                      ),
-                      onPressed: (lives > 0 && !wordGuessed.contains(letter)) ? () => guessLetter(letter) : null,
-                      child: Text(
-                        letter,
-                        style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    );
-                  }).toList(),
-                ),
+                Keyboard(onLetterPressed: _guessLetter,
+                    guessedLetters: _game.wordGuessed,
+                    lives: _game.lives),
               ],
             ),
           ),
-          if (isGameOver)
-            Positioned.fill(
-              child: Container(
-                color: Colors.black.withOpacity(0.8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    isWinner
-                        ? Lottie.asset('assets/winner.json', width: 200, height: 200)
-                        : Lottie.asset('assets/lose.json', width: 200, height: 200),
-                    SizedBox(height: 20),
-                    Text(
-                      isWinner ? "🎉 Gratulacje! Odgadłeś hasło! 🎉" : "💀 Przegrałeś! Hasło to: $wordToGuessByUser",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                        textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        foregroundColor: Colors.white,
-                      ),
-                      onPressed: isWinner ? () => Navigator.of(context).maybePop() : resetGame,
-                      child: isWinner ? Text("Powrót", style: TextStyle(fontSize: 18)) : Text("Zagraj ponownie", style: TextStyle(fontSize: 18)),
-                    )
-                  ],
-                ),
-              ),
-            ),
+          if (_game.isGameOver)
+            GameOverScreen(isWinner: _game.isWinner,
+                wordToGuess: _game.wordToGuess,
+                onRestart: _resetGame),
         ],
       ),
     );
