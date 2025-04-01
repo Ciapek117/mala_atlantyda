6eba99e407b136eb349f8cfde363f887545c1029
MartiHed <martyna.hedeszynska@gmail.com>
2025-04-01 22:45:54 +0200

    jeszcze lepsze CodeUnlockPage.dart i dodałam podpowiedzi wraz z alerDialogami

--- lib/pages/CodeUnlockPage.dart
+++ lib/pages/CodeUnlockPage.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,6 +25,14 @@ class _CodeUnlockScreenState extends State<CodeUnlockScreen> {
   final List<int> correctCode = [1, 9, 6, 3];
   bool isUnlocked = false;
   List<Color> colors = List.filled(4, Color(0xFFAFCBFF));
+  bool isDialogShown = false;
+  int attempts = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () => _showWelcomeDialog());
+  }
 
   void _increment(int index) {
     setState(() {
@@ -51,12 +60,19 @@ class _CodeUnlockScreenState extends State<CodeUnlockScreen> {
 
   void _checkCode() {
     setState(() {
+      attempts++;
+      PromtDialog(attempts);
+
       for (int i = 0; i < numbers.length; i++) {
         if (numbers[i] == correctCode[i]) {
           colors[i] = Colors.green;
 
-          if(_isCodeCorrect()){
+          if (_isCodeCorrect() && !isDialogShown) {
+            isDialogShown = true;
             isUnlocked = true;
+            Future.delayed(Duration(seconds: 2), () {
+              _showWinDialog();
+            });
           }
         } else if (correctCode.contains(numbers[i])) {
           colors[i] = Colors.orange;
@@ -66,6 +82,66 @@ class _CodeUnlockScreenState extends State<CodeUnlockScreen> {
       }
     });
   }
+
+  void PromtDialog(int attemptValue){
+    if(attemptValue == 3){
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.infoReverse,
+        animType: AnimType.scale,
+        title: "Podpowiedź pierwsza!",
+        desc: "tresc podpowiedzi 1",
+        btnOkOnPress: (){}
+      ).show();
+    } else if(attemptValue == 5){
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.infoReverse,
+        animType: AnimType.scale,
+        title: "Podpowiedź druga!",
+        desc: "tresc podpowiedzi 2",
+        btnOkOnPress: (){}
+      ).show();
+    } else if(attemptValue == 7){
+      AwesomeDialog(
+        context: context,
+        dialogType: DialogType.infoReverse,
+        animType: AnimType.scale,
+        title: "Podpowiedź trzecia!",
+        desc: "tresc podpowiedzi 3",
+        btnOkOnPress: (){}
+      ).show();
+    }
+  }
+
+
+  void _showWelcomeDialog(){
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.infoReverse,
+      animType: AnimType.scale,
+      title: 'Witaj w zgadywaniu kodu!',
+      desc: 'Spróbuj odgadnąć poprawny kod związany z miejscem, w którym się znajdujesz!',
+      btnOkText: 'Zaczynamy!',
+      btnOkOnPress: () {},
+    ).show();
+  }
+
+  void _showWinDialog() {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.success,
+      animType: AnimType.scale,
+      title: 'Gratulacje!',
+      desc: 'Udało się wszystko dopasować!',
+      btnOkText: 'Powrót',
+      btnOkOnPress: () {
+        Navigator.pop(context);
+
+      },
+    ).show();
+  }
+
 
   @override
   Widget build(BuildContext context) {
